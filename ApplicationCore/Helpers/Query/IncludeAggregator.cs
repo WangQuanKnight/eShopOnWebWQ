7e77823f10c309ac23f0fcb6c8fb5424f3a7797d
knight <w1520138768@outlook.com>
2020-04-19 12:31:30 +0800

    重写底层EF Core接口

--- ApplicationCore/Helpers/Query/IncludeAggregator.cs
+++ ApplicationCore/Helpers/Query/IncludeAggregator.cs
@@ -8,13 +8,13 @@ namespace ApplicationCore.Helpers.Query
 {
     public class IncludeAggregator<TEntity>
     {
-        public IncludeQuery Include<TProperty>(Expression<Func<TEntity,TProperty>> selector)
+        public IncludeQuery<TEntity,TProperty> Include<TProperty>(Expression<Func<TEntity,TProperty>> selector)
         {
             IncludeVisitor visitor = new IncludeVisitor();
             visitor.Visit(selector);
 
             Dictionary<IIncludeQuery, string> pathMap = new Dictionary<IIncludeQuery, string>();
-            IncludeQuery query = new IncludeQuery(pathMap);
+            IncludeQuery<TEntity,TProperty> query = new IncludeQuery<TEntity,TProperty>(pathMap);
 
             if (!string.IsNullOrEmpty(visitor.Path))
             {
---
+++ ApplicationCore/Specifications/CustomerOrdersWithItemsSpecification.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.Entities.OrderAggregate;
+using ApplicationCore.Helpers.Query;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Specifications
+{
+    public class CustomerOrdersWithItemsSpecification : BaseSpecification<Order>
+    {
+        public CustomerOrdersWithItemsSpecification(string buyerId)
+            : base(o => o.BuyerId == buyerId)
+        {
+            AddIncludes(query => query.Include(o => o.OrderItems).ThenInclude(i => i.CatalogItemOrdered));
+        }
+    }
+}
